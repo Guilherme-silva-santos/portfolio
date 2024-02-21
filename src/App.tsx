34b4295c984fbfbb1826dd4738c1354606b4bfab
Guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2024-02-21 13:30:01 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export function App() {
       <main className="flex flex-row items-center justify-around flex-wrap" >
         <div className="leading-relaxed">
           <h1 className="font-black flex-nowrap text-2xl">Hello, I'm Guilherme, front-end developer</h1>
-          <p className=" w-[415px] ">Former digital-marketing specialist, seeking to aplly competent development skills with focus on collaboration, communication and passion.</p>
+          <p className=" w-[415px] ">Former digital-marketing specialist, seeking to aplly competent development skills with focus on collaboration, communication and passion</p>
           <Button className="">Saiba mais<CaretRight/></Button>
         </div>
           <img className="size-72" src={avatar} />
